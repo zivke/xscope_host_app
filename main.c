5e0e7b37ac719e83a33bf6959c5a50fa49757a32
Nikola Zivkovic <zivke85@gmail.com>
2014-08-19 15:03:48 +0200

    Print integer data from the XScope port to system out

--- main.c
+++ main.c
@@ -2,12 +2,13 @@
 
 // Called with the registartions so that the app can map name->probe
 void hook_registration_received(int sockfd, int xscope_probe, char *name) {
-	printf("Socket file descriptor: %d; XScope probe number: %d; Name: %s", sockfd, xscope_probe, name);
+	printf("Socket file descriptor: %d; XScope probe number: %d; Name: %s\n", sockfd, xscope_probe, name);
 }
 
 // Called whenever data is received from the target
 void hook_data_received(int sockfd, int xscope_probe, void *data, int data_len) {
-
+	int *int_data = (int*)data;
+	printf("Socket file descriptor: %d; XScope probe number: %d; Data length: %d; Data:%d\n", sockfd, xscope_probe, data_len, int_data[0]);
 }
 
 // Called whenever the application is existing
